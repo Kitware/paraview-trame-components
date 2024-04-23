0eb08e94a17cedf71b5ee4ac876e62f30887fcb5
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-04-22 18:07:31 -0600

    feat(vuetify): expose widgets from ptc

--- ptc/__init__.py
+++ ptc/__init__.py
@@ -1,20 +1,10 @@
-from .colors import ColorBy
-from .core import Viewer
-from .palette import PalettePicker
-from .pipeline import PipelineBrowser
-from .utils import PARAVIEW_EXAMPLES, PARAVIEW_ROOT
-from .vcr import TimeControl
-from .cli import cli
+from .colors import ColorBy  # noqa
+from .core import Viewer  # noqa
+from .palette import PalettePicker  # noqa
+from .pipeline import PipelineBrowser  # noqa
+from .utils import PARAVIEW_EXAMPLES, PARAVIEW_ROOT  # noqa
+from .vcr import TimeControl  # noqa
+from .cli import cli  # noqa
+from .vuetify import *
 
 __version__ = "0.5.0"
-
-__all__ = [
-    "cli",
-    "Viewer",
-    "PipelineBrowser",
-    "ColorBy",
-    "TimeControl",
-    "PalettePicker",
-    "PARAVIEW_EXAMPLES",
-    "PARAVIEW_ROOT",
-]

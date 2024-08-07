c77e622645a803e42937b1abcdb3ed4bed794fcb
Sebastien Jourdain (Kitware) <sebastien.jourdain@kitware.com>
2024-08-07 17:54:34 -0600

    fix(apps): add sphere example for parallel rendering

---
+++ ptc/apps/sphere.py
@@ -0,0 +1,15 @@
+import paraview.web.venv  # noqa: F401
+from paraview import simple
+import ptc
+
+sphere = simple.Sphere()
+rep = simple.Show(sphere)
+view = simple.Render()
+
+simple.ColorBy(rep, ('POINTS', 'vtkProcessId'))
+
+rep.RescaleTransferFunctionToDataRange(True, False)
+rep.SetScalarBarVisibility(view, True)
+
+web_app = ptc.Viewer(from_state=True)
+web_app.start()

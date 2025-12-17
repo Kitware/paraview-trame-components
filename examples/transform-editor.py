48f76d80f799d3e507cac55090674310022373f5
Lucie Macron <lucie.macron@kitware.com>
2025-12-17 16:15:10 +0100

    feat(TransformEditor): Expose properties for customization
    Add a dataclass to handle each axis component definition and expose new properties

--- examples/transform-editor.py
+++ examples/transform-editor.py
@@ -10,6 +10,7 @@ sphere = simple.Sphere()
 sphere.Radius = 1.0
 simple.Show(sphere)
 
+# Will be updated
 box_source = simple.Box()
 simple.Show(box_source)
 simple.SetActiveSource(box_source)
@@ -18,11 +19,42 @@ web_app = Viewer()
 
 with web_app.ui:
     with web_app.col_left:
-        TransformEditor(
+        transform_editor = TransformEditor(
             show_translation=True,
             show_scale=True,
             show_origin=True,
             show_orientation=True,
+            show_apply_button=True,
         )
+
+        def z_scale_changed_2(_):
+            print("Z Scale changed 2")
+
+        transform_editor.typed_state.bind_changes(
+            {transform_editor.typed_state.name.scale.z.value: z_scale_changed_2}
+        )
+
+        transform_editor.typed_state.data.scale.x.visibility = False
+        transform_editor.typed_state.data.scale.y.visibility = False
+        transform_editor.typed_state.data.scale.z.control_variant = "stacked"
+        transform_editor.typed_state.data.translation.x.on_focus_lost_ctrl_callback_name = "on_focus_lost"
+
+        @web_app.ctrl.set("on_focus_lost")
+        def on_focus_lost():
+            # Example of how to apply a translation when focus is lost on translation's x value
+            transform_editor.apply_translation()
+            simple.ResetCamera()
+            web_app.ctrl.view_update()
+
+
+@web_app.ctrl.set("on_apply_clicked")
+def apply_clicked():
+    print("Apply button clicked")
+
+
+@web_app.state.change(transform_editor.typed_state.name.scale.z.value)
+def z_scale_changed(**kwargs):
+    print("Z Scale changed")
+
 
 web_app.start()
--- ptc/transform_editor.py
+++ ptc/transform_editor.py
@@ -1,16 +1,29 @@
+import logging
 from dataclasses import dataclass, field
 
 from paraview import simple
 
 from trame.widgets import vuetify3 as v3
+from trame_server.controller import FunctionNotImplementedError
 from trame_server.utils.typed_state import TypedState
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass()
+class AxisComponent:
+    value: float = 0.0
+    precision: int = 0
+    visibility: bool = True
+    control_variant: str = "hidden"
+    on_focus_lost_ctrl_callback_name: str = ""
 
 
 @dataclass
 class Transform:
-    x: float = 0.0
-    y: float = 0.0
-    z: float = 0.0
+    x: AxisComponent = field(default_factory=AxisComponent)
+    y: AxisComponent = field(default_factory=AxisComponent)
+    z: AxisComponent = field(default_factory=AxisComponent)
 
     ui_visibility: bool = True
 
@@ -22,6 +35,33 @@ class TransformEditorState:
     orientation: Transform = field(default_factory=Transform)
     origin: Transform = field(default_factory=Transform)
 
+    apply_button_visibility: bool = True
+
+
+class AxisComponentCol(v3.VCol):
+    def __init__(self, inner_state: TypedState[AxisComponent], **kwargs):
+        super().__init__(classes="pa-0", v_if=(inner_state.name.visibility,), **kwargs)
+
+        with self:
+            v3.VNumberInput(
+                classes="pa-1",
+                v_model=(inner_state.name.value,),
+                control_variant=(inner_state.name.control_variant,),
+                precision=(inner_state.name.precision,),
+                update_focused=(
+                    self.on_focus,
+                    f"[$event, {inner_state.name.on_focus_lost_ctrl_callback_name}]",
+                ),
+            )
+
+    def on_focus(self, focus: bool, on_focus_lost_callback_name: str) -> None:
+        if focus or not on_focus_lost_callback_name:
+            return
+        try:
+            self.server.controller[on_focus_lost_callback_name]()
+        except FunctionNotImplementedError:
+            logger.warning(f"{on_focus_lost_callback_name} is not implemented")
+
 
 class TransformEditorRow(v3.VRow):
     def __init__(self, label: str, inner_state: TypedState[Transform]):
@@ -31,32 +71,17 @@ class TransformEditorRow(v3.VRow):
             v_if=(inner_state.name.ui_visibility,),
         )
         with self:
-            v3.VCol(label, classes="pa-0")
-            with v3.VCol(classes="pa-0"):
-                v3.VNumberInput(
-                    label="x",
-                    v_model=(inner_state.name.x,),
-                    control_variant="hidden",
-                    precision=(5,),
-                )
-            with v3.VCol(classes="pa-0"):
-                v3.VNumberInput(
-                    label="y",
-                    v_model=(inner_state.name.y,),
-                    control_variant="hidden",
-                    precision=(5,),
-                )
-            with v3.VCol(classes="pa-0"):
-                v3.VNumberInput(
-                    label="z",
-                    v_model=(inner_state.name.z,),
-                    control_variant="hidden",
-                    precision=(5,),
-                )
+            v3.VCol(label, classes="pa-0", cols=3)
+
+            AxisComponentCol(inner_state=inner_state.get_sub_state(inner_state.name.x))
+            AxisComponentCol(inner_state=inner_state.get_sub_state(inner_state.name.y))
+            AxisComponentCol(inner_state=inner_state.get_sub_state(inner_state.name.z))
 
 
 def _convert_transform_to_list(transform: Transform) -> list[float]:
-    return [transform.x, transform.y, transform.z] if transform else []
+    return (
+        [transform.x.value, transform.y.value, transform.z.value] if transform else []
+    )
 
 
 class TransformEditor(v3.VCard):
@@ -66,21 +91,24 @@ class TransformEditor(v3.VCard):
         show_scale: bool = True,
         show_orientation: bool = True,
         show_origin: bool = True,
+        show_apply_button: bool = True,
         **kwargs,
     ):
         super().__init__(width="100%", **kwargs)
 
         # Initialize component state
         self.typed_state = TypedState(self.state, TransformEditorState)
+
+        self.typed_state.data.scale.x.value = 1.0
+        self.typed_state.data.scale.y.value = 1.0
+        self.typed_state.data.scale.z.value = 1.0
+
         self.typed_state.data.translation.ui_visibility = show_translation
-
         self.typed_state.data.scale.ui_visibility = show_scale
-        self.typed_state.data.scale.x = 1.0
-        self.typed_state.data.scale.y = 1.0
-        self.typed_state.data.scale.z = 1.0
-
         self.typed_state.data.orientation.ui_visibility = show_orientation
         self.typed_state.data.origin.ui_visibility = show_origin
+
+        self.typed_state.data.apply_button_visibility = show_apply_button
 
         self._build_ui()
 
@@ -114,8 +142,7 @@ class TransformEditor(v3.VCard):
                         self.typed_state.name.orientation
                     ),
                 )
-
-            with v3.VCardActions():
+            with v3.VCardActions(v_if=(self.typed_state.name.apply_button_visibility,)):
                 v3.VSpacer()
                 v3.VBtn("Apply", click=self.apply_changes)
 
@@ -129,24 +156,28 @@ class TransformEditor(v3.VCard):
 
     def _get_display_properties(self):
         if self.source_proxy is None or self.view_proxy is None:
-            print("Cannot compute display properties")
+            logger.warning(
+                "Cannot compute display properties. Missing source proxy or view proxy."
+            )
             return None
         return simple.GetDisplayProperties(self.source_proxy, view=self.view_proxy)
 
     def apply_changes(self):
-        self.apply_translation(
-            _convert_transform_to_list(self.typed_state.data.translation)
-        )
-        self.apply_origin(_convert_transform_to_list(self.typed_state.data.origin))
-        self.apply_orientation(
-            _convert_transform_to_list(self.typed_state.data.orientation)
-        )
-        self.apply_scale(_convert_transform_to_list(self.typed_state.data.scale))
+        self.apply_translation()
+        self.apply_origin()
+        self.apply_orientation()
+        self.apply_scale()
 
         simple.ResetCamera()
         self.ctrl.view_update()
 
-    def apply_translation(self, translation):
+        try:
+            self.server.controller.on_apply_clicked()
+        except FunctionNotImplementedError:
+            logger.warning("on_apply_clicked is not implemented")
+
+    def apply_translation(self):
+        translation = _convert_transform_to_list(self.typed_state.data.translation)
         display_properties = self._get_display_properties()
         if display_properties is None:
             return
@@ -154,7 +185,8 @@ class TransformEditor(v3.VCard):
         display_properties.Translation = translation
         display_properties.PolarAxes.Translation = translation
 
-    def apply_orientation(self, orientation):
+    def apply_orientation(self):
+        orientation = _convert_transform_to_list(self.typed_state.data.orientation)
         display_properties = self._get_display_properties()
         if display_properties is None:
             return
@@ -162,14 +194,16 @@ class TransformEditor(v3.VCard):
         display_properties.Orientation = orientation
         display_properties.PolarAxes.Orientation = orientation
 
-    def apply_origin(self, origin):
+    def apply_origin(self):
+        origin = _convert_transform_to_list(self.typed_state.data.origin)
         display_properties = self._get_display_properties()
         if display_properties is None:
             return
 
         display_properties.Origin = origin
 
-    def apply_scale(self, scale):
+    def apply_scale(self):
+        scale = _convert_transform_to_list(self.typed_state.data.scale)
         display_properties = self._get_display_properties()
         if display_properties is None:
             return
